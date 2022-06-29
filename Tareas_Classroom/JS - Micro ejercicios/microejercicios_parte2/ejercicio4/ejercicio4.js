58128ec5a542383899805a559074e15483068e61
prlongoria <prlongoria@gmail.com>
2022-06-29 21:05:07 +0200

    ejercicios 4 y 5 de la segunda parte

--- Tareas_Classroom/JS - Micro ejercicios/microejercicios_parte2/ejercicio4/ejercicio4.js
+++ Tareas_Classroom/JS - Micro ejercicios/microejercicios_parte2/ejercicio4/ejercicio4.js
@@ -1,27 +1,15 @@
 //Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
 const button = document.getElementById("button");
 button.onclick = function getNumberOfLetterA (){
-    
+ 
     let phrase = document.getElementById("phraseWithA").value;
-    console.log (phrase);
     let lettersOfPhrase =[];
-    //console.log (numberOfLetters);
-    
-    
+        
     for ( let i = 0; i < phrase.length; i++) {
         if (phrase[i].toLowerCase() === "a") lettersOfPhrase.push(i);
-    }
-    
+    }   
 
 document.getElementById("numberOfLettersA").innerHTML = "Esta frase contiene" + " " + lettersOfPhrase.length + " " + "letras A";
 
 }
-/*var cadena = "Cuantas 'A' aparecen en esta cadena."
-
-var indices = [];
-for(var i = 0; i < cadena.length; i++) {
-	if (cadena[i].toLowerCase() === "a") indices.push(i);
-}
-
-document.getElementById("resultado").innerHTML = indices.length;*/
 
---
+++ Tareas_Classroom/JS - Micro ejercicios/microejercicios_parte2/ejercicio5/ejercicio5.js
@@ -0,0 +1,60 @@
+//Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+let button = document.getElementById("button");
+button.onclick= function getVowelsOfPhrase() {
+    let vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+    let phrase = document.getElementById("phraseInput").value; //recojo lo que pone el usuario en el input
+    let arrayPhrase = phrase.split(""); //convierto la phrase (string) en un array
+    let arrayOfVowels = [];
+
+    for (var i = 0; i < arrayPhrase.length; i++) {
+        let arrayPhraseElement = arrayPhrase[i];
+
+        if (vowels.includes(arrayPhraseElement)) {
+            arrayOfVowels.push(arrayPhraseElement);
+        }
+        //return arrayOfVowels;
+       
+    }
+    document.getElementById('vowels').innerHTML = arrayOfVowels;
+
+
+
+    /*if (arrayPhrase.includes(vowels)) {
+        let arrayVowelsInPhrase = arrayPhrase
+        document.getElementById ('vowels').innerHTML = arrayVowelsInPhrase;
+    }*/
+}
+
+    /*let result = arrayPhrase.filter (element => {
+        if (element==vowels){
+            return true;
+        }
+        return false;
+    })
+    document.getElementById('vowels').innerHTML= result;
+    console.log(result);*/
+
+    /*for (let i in vowels){
+        for (let j in arrayPhrase){
+            if(vowels[i] == arrayPhrase[j]){
+                
+                console.log(arrayPhrase[j])
+             }else{
+                console.log('wrong');
+             }
+        }
+    }*/
+
+    //let vowelsOfPhrase = phrase.match(vowels);
+
+    //console.log (vowelsOfPhrase);
+   // if (vowelsOfPhrase[i].toLowerCase === vowels
+    
+    
+
+    
+    
+    /*for ( let i = 0; i < phrase.length; i++) {
+        if (phrase[i] === vowels[j]) vowelsOfPhrase.push(i);
+    } */
