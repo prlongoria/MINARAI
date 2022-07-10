fcb48547983d81c8ffab94ff3595ca7fb261a7e5
prlongoria <prlongoria@gmail.com>
2022-07-10 13:20:54 +0200

    ejercicio 8 completo

--- Tareas_Classroom/JS - Micro ejercicios/microejercicios_parte2/ejercicio8/ejercicio8.js
+++ Tareas_Classroom/JS - Micro ejercicios/microejercicios_parte2/ejercicio8/ejercicio8.js
@@ -1,2 +1,25 @@
-Escribe un programa que pida un número y nos diga si es divisible 
-por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+/*Escribe un programa que pida un número y nos diga si es divisible 
+por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)*/
+const button = document.getElementById ("button");
+button.onclick = function isDivisibleBy ()  {
+    const number = document.getElementById ("userNumber").value;
+    let num2 = 2;
+    let num3 = 3;
+    let num5 = 5;
+    let num7 = 7;
+    if (number % num2 === 0 ){
+        console.log( number +" "+"Is divisible by "+" " + "2");
+        document.getElementById("isDivisibleBy").innerHTML = number +" "+"Is divisible by "+" " + "2";
+    }
+
+    if (number % num3 === 0) {
+        document.getElementById("isDivisibleBy").innerHTML = number +" "+"Is divisible by "+" " + num3;
+    }
+    if (number % num5 === 0) {
+        document.getElementById("isDivisibleBy").innerHTML = number +" "+"Is divisible by "+" " + num5;
+    }
+    if (number % num7 === 0) {
+        document.getElementById("isDivisibleBy").innerHTML = number +" "+"Is divisible by "+" " + num7;
+    }
+
+}
